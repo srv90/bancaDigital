3abb3b537d0e71c1e344e199ca334566161b5aa6
Enrique Mendioroz <emendioroz@bna.com.ar>
2025-08-16 15:29:17 -0300

    chromedriver manual

--- src/test/java/ar/com/bna/digitaltest/testcases/BaseTest.java
+++ src/test/java/ar/com/bna/digitaltest/testcases/BaseTest.java
@@ -145,7 +145,7 @@ public class BaseTest {
 		} else {
 			options.addArguments("--force-device-scale-factor=0.9");
 		}
-	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\E97782\\Downloads\\chromedriver-win64\\chromedriver.exe");
+	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\n67814\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
 
 		return new ChromeDriver(options);
